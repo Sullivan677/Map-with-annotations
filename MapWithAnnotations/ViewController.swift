d3d42fc70a6df1b78d254d8e90d14a7d084210a5
Sullivan677 <decarli.sulli@gmail.com>
2021-01-22 09:37:39 +0100

    Initial Commit

---
+++ MapWithAnnotations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapWithAnnotations
+//
+//  Created by Sullivan De carli on 22/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
